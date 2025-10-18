5c37fd9faab92b06d8c7327062ec1228f23c6b15
grant <abernathy.grant@icloud.com>
2025-10-18 15:43:22 -0500

    Build config

--- Source/Ken/Core/Build.hh
+++ Source/Ken/Core/Build.hh
@@ -1,1 +1,21 @@
+//=================================================================================================
+// Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+// Copyright Against All Odds. All rights reserved.
+//=================================================================================================
 #pragma once
+
+#define __DEBUG         0
+#define __DEV           0
+#define __PROFILE       0
+#define __BETA          0
+#define __QA            0
+
+#define __RELEASE       0
+#define __RETAIL        0
+#define __FINAL         0
+#define __OPTIMIZED     0
+
+#define __LARGE_SCENES  0
+#define __DP_PHYS       0
+#define __VR            0
+#define __AR            0

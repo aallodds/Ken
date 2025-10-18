e1663017c933c629b8648b23487933022323d0b6
grant <abernathy.grant@icloud.com>
2025-10-18 13:08:16 -0500

    Base files

---
+++ Source/Ken/Core/Compiler.hh
@@ -0,0 +1,14 @@
+//=================================================================================================
+// Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+// Copyright Against All Odds. All rights reserved.
+//=================================================================================================
+#pragma once
+
+#define __MSVC              0   // Microsoft Visual C++
+#define __CLANG             0   // Clang/LLVM
+#define __GCC               0   // GNU Compiler Collection
+#define __INTEL             0   // Intel Compiler
+#define __WATCOM            0   // Watcom C/C++
+#define __BORLAND           0   // Borland C++
+#define __EMSCRIPTEN        0   // Emscripten Compiler
+#define __NVCC              0   // NVIDIA CUDA Compiler
---
+++ Source/Ken/Core/Top.hh
@@ -0,0 +1,17 @@
+//=================================================================================================
+// Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+// Copyright Against All Odds. All rights reserved.
+//=================================================================================================
+#pragma once
+
+// Please do not change the order of these includes.
+#include "Ken/Core/Compiler.hh"
+#include "Ken/Core/Architecture.hh"
+#include "Ken/Core/SIMD.hh"
+#include "Ken/Core/Platform.hh"
+#include "Ken/Core/Build.hh"
+#include "Ken/Core/Targets.hh"
+
+#include "Ken/Core/Types.hh"
+#include "Ken/Core/Macros.hh"
+#include "Ken/Core/UtilityFunctions.hh"

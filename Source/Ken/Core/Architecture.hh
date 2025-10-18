48302e7fe2aaefb225c62986327325266807ed75
grant <abernathy.grant@icloud.com>
2025-10-18 13:09:49 -0500

    Architectures

--- Source/Ken/Core/Architecture.hh
+++ Source/Ken/Core/Architecture.hh
@@ -1,1 +1,19 @@
+//=================================================================================================
+// Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+// Copyright Against All Odds. All rights reserved.
+//=================================================================================================
 #pragma once
+
+#define __X86_32    0   // x86 32-bit (IA-32)
+#define __X86_64    0   // x86 64-bit (x86-64, AMD64)
+#define __ARM32     0   // ARM 32-bit (AArch32)
+#define __ARM64     0   // ARM 64-bit (AArch64)
+#define __PPC32     0   // PowerPC 32-bit
+#define __PPC64     0   // PowerPC 64-bit
+#define __MIPS32    0   // MIPS 32-bit
+#define __MIPS64    0   // MIPS 64-bit
+#define __CELL      0   // Cell Broadband Engine PlayStation(3)
+#define __PPU       0   // Power Processing Unit (PPU) of PlayStation(3)
+#define __SPU       0   // Synergistic Processing Unit (SPU) of PlayStation(3)
+#define __EE        0   // Emotion Engine (EE) of PlayStation 2
+#define __IOP       0   // I/O Processor (IOP) of PlayStation 2

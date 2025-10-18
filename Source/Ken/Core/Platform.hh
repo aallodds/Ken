c6f9e2cfcd80f54ce6248936304edfbe401f228b
grant <abernathy.grant@icloud.com>
2025-10-18 15:43:26 -0500

    Platform

--- Source/Ken/Core/Platform.hh
+++ Source/Ken/Core/Platform.hh
@@ -1,1 +1,38 @@
+//=================================================================================================
+// Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+// Copyright Against All Odds. All rights reserved.
+//=================================================================================================
 #pragma once
+
+#define __DESKTOP           0   // Desktop platform
+#define __MOBILE            0   // Mobile platform
+#define __CONSOLE           0   // Console platform
+
+#define __WINDOWS           0   // Any windows platform
+#define __WIN32             0   // Windows 32-bit
+#define __WIN64             0   // Windows 64-bit
+
+#define __MACOS             0   // Any macOS platform
+#define __MACOS_X86         0   // macOS x86_64
+#define __MACOS_ARM         0   // macOS ARM64
+
+#define __LINUX             0   // Any Linux platform
+#define __LINUX32           0   // Linux 32-bit
+#define __LINUX64           0   // Linux 64-bit
+#define __LINUX_ARM32       0   // Linux ARM 32-bit
+#define __LINUX_ARM64       0   // Linux ARM 64-bit
+
+#define __IOS               0   // Any iOS platform
+#define __ANDROID           0   // Any Android platform
+
+#define __SONY_PLATFORM     0   // Sony platforms
+#define __PS2               0   // PlayStation 2
+#define __PS3               0   // PlayStation 3
+#define __ORBIS             0   // PlayStation 4
+#define __PROSPERO          0   // PlayStation 5
+
+#define __XBOX_PLATFORM     0   // Xbox platforms
+#define __XBOX              0   // Original XBOX
+#define __XENON             0   // Xbox 360
+#define __DURANGO           0   // Xbox One
+#define __SCARLETT          0   // Xbox Series X/S

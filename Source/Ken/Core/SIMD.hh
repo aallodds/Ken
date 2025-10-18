a897d4a78930b59a169b57a0dcdad76b387d9c65
grant <abernathy.grant@icloud.com>
2025-10-18 15:43:33 -0500

    SIMD

--- Source/Ken/Core/SIMD.hh
+++ Source/Ken/Core/SIMD.hh
@@ -1,1 +1,20 @@
+//=================================================================================================
+// Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+// Copyright Against All Odds. All rights reserved.
+//=================================================================================================
 #pragma once
+
+#define __SSE             0     // Streaming SIMD Extensions
+#define __SSE2            0     // Streaming SIMD Extensions 2
+#define __SSE3            0     // Streaming SIMD Extensions 3
+#define __SSSE3           0     // Supplemental Streaming SIMD Extensions 3
+#define __SSE4_1          0     // Streaming SIMD Extensions 4.1
+#define __SSE4_2          0     // Streaming SIMD Extensions 4.2
+#define __AVX             0     // Advanced Vector Extensions
+#define __AVX2            0     // Advanced Vector Extensions 2
+#define __AVX512          0     // Advanced Vector Extensions 512
+#define __NEON            0     // ARM NEON
+#define __ALTIVEC         0     // AltiVec / VMX
+#define __VSX             0     // Vector Scalar eXtension
+#define __MMX             0     // MultiMedia eXtensions
+#define __FMA             0     // Fused Multiply-Add
